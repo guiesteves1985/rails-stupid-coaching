219b6ccf86df33763f6df60c015278213b4d9030
Guilherme Esteves <guiesteves1985@gmail.com>
2019-08-19 14:35:55 -0300

    exercise 2 done without css

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'pages/questions'
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'ask', to: '#ask', as: :ask
+  get 'ask', to: 'questions#ask'
+
+  get 'answer', to: 'questions#answer'
 end
